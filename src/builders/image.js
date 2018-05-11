8aea005ae35dccdd435824c49ed1b1e3c222bedf
asincai <alexandru.sincai@endava.com>
2018-05-11 16:17:25 +0300

    Images

--- src/builders/image.js
+++ src/builders/image.js
@@ -1,5 +1,13 @@
+import colr from 'colr';
+
 export default class Image {
 	constructor( obj ) {
-		this.obj = obj
+		this.width = Number( obj.width_o );
+		this.height = Number( obj.height_o );
+		this.src = obj.url_o;
+		let h = Math.floor( Math.random() * 60 );
+		let s = Math.floor( Math.random() * 20 ) + 20;
+		let l = Math.random( Math.random() * 5 ) + 90;
+		this.color = colr.fromHsl( h, s, l ).toHex();
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import '../node_modules/reset-css/reset.css';
 import './index.css';
 
 import secret from './secret';
+import Image from './builders/image';
+// import Chapter from './builders/chapter';
 
 class CodexMysterium extends React.Component {
 	constructor() {
@@ -16,26 +19,72 @@ class CodexMysterium extends React.Component {
 				'flowers',
 				'insects',
 				'architecture'
-			]
+			],
+			images: []
 		}
 
 		this.loadImages = this.loadImages.bind( this );
 	}
 
 	loadImages() {
-		let loadedImages = localStorage.getItem( 'loadedImages' );
+		let loadedImages = localStorage.getItem( 'CodexMysteriumLoadedImages' );
 		if ( loadedImages ) {
 			this.setState( {
 				images: JSON.parse( loadedImages )
 			} );
+			return;
 		} else {
+			let images = {};
+			let searchUrls = [];
 
+			for ( let i in this.state.sections ) {
+				const searchOptions = Object.entries( {
+					api_key: secret.api_key,
+					method: 'flickr.photos.search',
+					text: this.state.sections[ i ],
+					tags: 'bookcentury1700,bookcentury1600,bookcentury',
+					tag_mode: 'any',
+					license: '7,9,10',
+					format: 'json',
+					nojsoncallback: 1,
+					extras: 'url_o,url_m',
+					per_page: 5,
+					sort: 'relevance',
+				} ).map( o => o.join( '=' ) ).join( '&' );
+
+				searchUrls.push( `https://api.flickr.com/services/rest/?${ searchOptions }` );
+			}
+			axios.all( searchUrls.map( l => axios.get( l ) ) ).then( axios.spread( ( ...res ) => res.forEach( ( r, j ) => {
+				images[ this.state.sections[ j ] ] = [];
+				r.data.photos.photo.forEach( i => {
+					images[ this.state.sections[ j ] ].push( new Image( i ) );
+				} );
+			} ) ) ).then( () => {
+
+				// let subjects = this.state.section_names.sort( () => Math.random() > 0.5 );
+				// const chapters = Array( Math.floor( Math.random() * 2 ) + 1 ).fill().map( ( _, i, a ) => {
+				// return new Chapter( subjects[ i ], section_images[ subjects[ i ] ] );
+				// } );
+
+				localStorage.setItem( 'CodexMysteriumLoadedImages', JSON.stringify( images ) );
+				this.setState( {
+					images: images,
+					// chapters: chapters
+				} );
+			} );
 		}
+	}
+
+	componentWillMount() {
+		this.loadImages();
 	}
 
 	render() {
 		return (
 			<React.Fragment>
+        <pre>
+          <code>{ JSON.stringify( this.state, null, '\t' ) }</code>
+        </pre>
         <ul>
           <li>Load images</li>
           <li>Build chapters</li>
